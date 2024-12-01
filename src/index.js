b990a9ff83ed9036ce1c793c3a77b2e40f4823da
auborealis-cyber <149447678+auborealis-cyber@users.noreply.github.com>
2024-12-01 13:13:06 -0800

    Updated website

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 alert("Welcome!");
+
+function updateCert(event) {
+    let certification = event.target.value;
+    if (certification === firstAid) {
+        let certificationElement = document.querySelector("#more-info");
+        certificationElement.innerHTML = "Trained by American Red Cross in order to be prepared for emergency situations";
+    }
+}
+
+
+let certificationSelectElement = document.querySelector("#certifications");
+certificationSelectElement.addEventListener("change", updateCert);
